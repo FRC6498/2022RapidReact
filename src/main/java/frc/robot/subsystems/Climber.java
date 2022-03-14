685a9ff7fecc60284b6feeeac42bc717f0182d8d
DeltaDizzy <EJBraswell@gmail.com>
2022-03-13 23:12:30 -0500

    remove closed loop stuff from climber until we figure it out

--- src/main/java/frc/robot/subsystems/Climber.java
+++ src/main/java/frc/robot/subsystems/Climber.java
@@ -19,32 +19,25 @@ import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import io.github.oblarg.oblog.Loggable;
 import io.github.oblarg.oblog.annotations.Log;
 
-public class Climber extends SubsystemBase implements Loggable{
+public class Climber extends SubsystemBase {
   /** Creates a new Climber. */
   WPI_TalonFX climberMotor;
-  Solenoid pinPull;
+  Solenoid lock;
+  boolean enabled;
   TalonFXConfiguration config;
-  double climberMotorSetpoint;
-  public boolean isDown = false;
-
-  boolean predeploy = true;
   double input;
-  //private final StallDetector climberStall;
   public Climber() {
+    enabled = false;
     config = new TalonFXConfiguration();
-    isDown = true;
-    //climberStall = new StallDetector(new PDPSlot(new PDP(), PDPPortNumber.Port8, PDPBreaker.TwentyAmp));
-    //climberStall.setStallCurrent(40);
-    //climberStall.setMinStallMillis(100);
     climberMotor = new WPI_TalonFX(climberMotorCANId);
-    config.closedloopRamp = 1;
+    config.closedloopRamp = 0.5;
     config.peakOutputForward = 0.75;
     config.peakOutputReverse = -0.75;
     climberMotor.setInverted(false);
     config.forwardLimitSwitchSource = LimitSwitchSource.FeedbackConnector;
     config.forwardLimitSwitchNormal = LimitSwitchNormal.NormallyOpen;
     config.clearPositionOnLimitF = true;
-    
+
     //setup encoders
     config.slot0.kP = climber_kP;
     climberMotor.configAllSettings(config);
@@ -66,49 +59,16 @@ public class Climber extends SubsystemBase implements Loggable{
     this.input = input;
   }
 
-  // 1 inch = 11660 ticks
-  int ticksPerInch = 11660;
-  public void lowerClimber()  {
-    climberMotorSetpoint = 0;
-    isDown = true;
-  }
-
-  public void raiseClimber() {
-    climberMotorSetpoint = 21 * ticksPerInch;
-    isDown = false;
-  }
-  //@Log
-  public double whereClimber(double getEncoderPosition) {
-    if (getEncoderPosition < 50) {
-      isDown = true;
-    } else {
-      isDown = false;
-    }
-    return getEncoderPosition;
-  }
-
-  public void toggleClimber() {
-    if (isDown) {
-      raiseClimber();
-    } else {
-      lowerClimber();
-    }
-  }
-
-  @Log.BooleanBox(name = "Climb Complete")
-  private boolean getClimbed() {
-    return false;//return climberStall.getStallStatus().isStalled == true;
-  }
-
   @Override
   public void periodic() {
-    whereClimber(getEncoderPosition());
-    //climberStall.updateStallStatus();
-    if (getClimbed()) {
-      input = 0;
-
+    if (enabled) {
+      climberMotor.set(ControlMode.PercentOutput, input);
+    } else {
+      if (climberMotor.get() > 0) {
+        climberMotor.set(0);
+      }
     }
-    climberMotor.set(ControlMode.PercentOutput, input);
+    
 
     //climberMotor.set(ControlMode.Position, climberMotorSetpoint);
     // This method will be called once per scheduler run
@@ -118,3 +78,4 @@ public class Climber extends SubsystemBase implements Loggable{
     climberMotor.setStatusFramePeriod(StatusFrame.Status_2_Feedback0, 250);
   }
 }
+//TODO: remove all periodic motor sets
