6a6a9831e3591e32ddf2747fefe01ffeb1bf0866
muffins <73028463+classified-rat@users.noreply.github.com>
2022-04-10 11:59:47 -0500

    added comments to code

--- led arduino code.cpp
+++ led arduino code.cpp
@@ -1,9 +1,11 @@
 //binary mappings
+//each index represents a color
 int bval [4] = {0b100,0b010,0b001,0b101};
 
 void setup() {
   // put your setup code here, to run once:
 
+  //sets pins 2-4 to rgb output
   pinMode(2,OUTPUT); // r
   pinMode(3,OUTPUT); // g
   pinMode(4,OUTPUT); // b
@@ -27,17 +29,25 @@ void loop() {
 }
 
 void rgb(int rgb){
+  //rgb values are binary and digital
+
+  // bit 1 = r
+  // bit 2 = g
+  // bit 3 = b
   int r = rgb & 0b100;
   int g = rgb & 0b010;
   int b = rgb & 0b001;
 
+  // convert binary values to pin values
+  // inverts values because ground = on
   r = r >= 1 ? LOW : HIGH;
   g = g >= 1 ? LOW : HIGH;
   b = b >= 1 ? LOW : HIGH;
 
+  //write the values to the pins
   digitalWrite(2, r); // r
-  digitalWrite(4, g); // g
-  digitalWrite(5, b); // b
+  digitalWrite(3, g); // g
+  digitalWrite(4, b); // b
 
 
 }
